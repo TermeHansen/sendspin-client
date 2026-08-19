ad498a9927dcd990f2f8fcb84685a84868090df7
TermeHansen <termopeten@gmail.com>
2026-08-19 22:41:22 +0200

    find libatomic

--- cmake/armv6-toolchain.cmake
+++ cmake/armv6-toolchain.cmake
@@ -5,12 +5,12 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR armv6l)
 
-# Compiler flags for ARMv6 (Raspberry Pi 1 CPU: arm1176jzf-s)
-# -mcpu=arm1176jzf-s: Optimize for the specific CPU in Raspberry Pi 1
+# Compiler flags for ARMv6 (Raspberry Pi 1 and other ARMv6 processors)
 # -march=armv6: Target ARMv6 architecture
+# -marm: Use ARM instruction set (not Thumb) - required for ARMv6 with hard-float
 # -mfpu=vfp: Use VFP floating point unit
-# -mfloat-abi=hard: Use hardware floating point
-set(ARMv6_COMPILE_FLAGS "-mcpu=arm1176jzf-s -march=armv6 -mfpu=vfp -mfloat-abi=hard")
+# -mfloat-abi=hard: Use hardware floating point ABI
+set(ARMv6_COMPILE_FLAGS "-march=armv6 -marm -mfpu=vfp -mfloat-abi=hard")
 
 set(CMAKE_C_FLAGS "${ARMv6_COMPILE_FLAGS}" CACHE INTERNAL "C compiler flags for ARMv6")
 set(CMAKE_CXX_FLAGS "${ARMv6_COMPILE_FLAGS}" CACHE INTERNAL "C++ compiler flags for ARMv6")
@@ -22,18 +22,41 @@ set(CMAKE_MODULE_LINKER_FLAGS "${ARMv6_COMPILE_FLAGS}" CACHE INTERNAL "Module li
 
 # Link libatomic (required for ARMv6 as it lacks hardware atomic instructions)
 # For ARMv6, we always need to link -latomic
-message(STATUS "ARMv6: Adding -latomic to linker flags")
-set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -latomic" CACHE INTERNAL "Executable linker flags with atomic")
-set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -latomic" CACHE INTERNAL "Shared linker flags with atomic")
-set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -latomic" CACHE INTERNAL "Module linker flags with atomic")
+message(STATUS "ARMv6: Configuring libatomic support")
 
-# Also try to find the library for potential additional paths
-find_library(LIBATOMIC atomic)
+# Add common ARM library paths to help find libatomic
+list(APPEND CMAKE_PREFIX_PATH /usr/lib/arm-linux-gnueabihf /usr/lib/gcc/arm-linux-gnueabihf/14)
+
+# Find libatomic and add its directory to the link path
+find_library(LIBATOMIC atomic 
+    PATHS /usr/lib/arm-linux-gnueabihf /usr/lib/gcc/arm-linux-gnueabihf/14
+    NO_DEFAULT_PATH
+)
+
 if(LIBATOMIC)
     message(STATUS "Found libatomic at: ${LIBATOMIC}")
+    # Get the directory containing libatomic
+    get_filename_component(LIBATOMIC_DIR ${LIBATOMIC} DIRECTORY)
+    # Add the directory to the linker search path
+    set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L${LIBATOMIC_DIR}" CACHE INTERNAL "Linker flags with libatomic path")
+    set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -L${LIBATOMIC_DIR}" CACHE INTERNAL "Linker flags with libatomic path")
 else()
-    message(STATUS "libatomic not found via find_library, but -latomic flag added to linker")
+    # Fallback: try without path restriction
+    find_library(LIBATOMIC atomic)
+    if(LIBATOMIC)
+        message(STATUS "Found libatomic at: ${LIBATOMIC}")
+        get_filename_component(LIBATOMIC_DIR ${LIBATOMIC} DIRECTORY)
+        set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -L${LIBATOMIC_DIR}" CACHE INTERNAL "Linker flags with libatomic path")
+        set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -L${LIBATOMIC_DIR}" CACHE INTERNAL "Linker flags with libatomic path")
+    else()
+        message(WARNING "libatomic not found, but -latomic will be added to linker flags")
+    endif()
 endif()
+
+# Always add -latomic to link against the library
+set(CMAKE_EXE_LINKER_FLAGS "${CMAKE_EXE_LINKER_FLAGS} -latomic" CACHE INTERNAL "Linker flags with atomic library")
+set(CMAKE_SHARED_LINKER_FLAGS "${CMAKE_SHARED_LINKER_FLAGS} -latomic" CACHE INTERNAL "Linker flags with atomic library")
+set(CMAKE_MODULE_LINKER_FLAGS "${CMAKE_MODULE_LINKER_FLAGS} -latomic" CACHE INTERNAL "Linker flags with atomic library")
 
 # Set find root path for cross-compilation (if needed)
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
